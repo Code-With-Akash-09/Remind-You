1078d5367f15cccfcd7e93b16b856d9b92561f83
Akash09082001 <aakashl@letsupgrade.in>
2025-05-15 18:09:37 +0530

    feat: Refactor Home component layout for improved responsiveness and visual structure

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -5,42 +5,46 @@ import Link from "next/link"
 const Home = () => {
 	return (
 		<>
-			<div className="flex size-full">
-				<div className="flex flex-col w-full flex-1 max-w-7xl mx-auto items-center justify-center px-4 py-2">
-					<div className="grid grid-cols-1 flex-1 md:grid-cols-2 gap-4">
-						<div className="flex flex-col gap-6 md:gap-8 w-full items-start justify-center">
-							<div className="flex flex-col gap-4 w-fit">
-								<h1 className="text-4xl md:text-5xl font-bold text-blue-600 dark:text-gray-50">
-									Build your next Ideas
-								</h1>
-								<div className="flex w-ful">
-									<p className="text-lg md:text-xl text-slate-500 dark:text-slate-300">
+			<div className="flex size-full p-4 md:p-6 overflow-x-auto">
+				<div className="flex flex-col h-fit w-full gap-6">
+					<div className="flex flex-col w-full h-[600px] md:h-auto md:flex-1 lg:max-h-[500px] max-w-7xl mx-auto items-center md:items-start justify-center p-4 py-2 border border-neutral-200 dark:border-neutral-800 rounded-3xl bg-gradient-to-r from-amber-400/20 to-blue-500/20 dark:from-blue-600/10 dark:to-amber-600/10">
+						<div className="grid grid-cols-1 flex-1 md:grid-cols-2 gap-4">
+							<div className="flex flex-col gap-6 md:gap-8 w-full items-center md:items-start justify-center md:p-8 lg:p-16 xl:p-24">
+								<div className="flex flex-col gap-4 w-fit mx-auto md:mx-0">
+									<h1 className="flex text-4xl md:text-5xl font-bold text-blue-600 dark:text-gray-50 w-full text-center md:text-left">
+										Build your next Ideas
+									</h1>
+									<p className="text-lg md:text-xl text-slate-500 dark:text-slate-300 text-center md:text-left">
 										Remind You is a free and open-source
 										application that allows you to create
 										and manage your own to-do lists.
 									</p>
 								</div>
+								<div className="flex w-fit gap-4">
+									<Button
+										asChild
+									>
+										<Link href={"/dashboard"}>
+											Get Started
+										</Link>
+									</Button>
+								</div>
 							</div>
-							<div className="flex w-fit gap-4">
-								<Button
-									asChild
-								>
-									<Link href={"/dashboard"}>
-										Get Started
-									</Link>
-								</Button>
+							<div className="flex w-full">
+								<div className="flex w-full aspect-video relative">
+									<Image
+										src={"/assets/banner-img/hero-banner.avif"}
+										alt="Hero banner"
+										fill
+										className="object-contain rounded-lg"
+									/>
+								</div>
 							</div>
 						</div>
-						<div className="flex w-full">
-							<div className="flex w-full aspect-video relative">
-								<Image
-									src={"/assets/banner-img/hero-banner.avif"}
-									alt="Hero banner"
-									fill
-									className="object-contain rounded-lg"
-								/>
-							</div>
-						</div>
+					</div>
+
+					<div className="flex w-full rounded-3xl bg-white border border-neutral-200 dark:border-neutral-800 max-w-7xl mx-auto h-40">
+
 					</div>
 				</div>
 			</div>
